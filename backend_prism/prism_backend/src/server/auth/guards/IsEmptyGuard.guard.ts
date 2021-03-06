7f8fca7ba83661a5e21ab1581a8145a141ee7d4f
Dmytro <turovetsd@icloud.com>
2021-03-06 15:28:45 +0200

    before commit

--- backend_prism/prism_backend/src/server/auth/guards/IsEmptyGuard.guard.ts
+++ backend_prism/prism_backend/src/server/auth/guards/IsEmptyGuard.guard.ts
@@ -1,9 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { CreateUserDto } from 'src/server/users/dto/create-user.dto';
+import { IUser } from 'src/server/users/iuser.interface';
+
+
+
 
 @Injectable()
 export class IsEmptyGuard implements CanActivate {
 
+	
 	canActivate(context: ExecutionContext): boolean | Promise<boolean> {
 
 		console.log("I am in Empty guard");
@@ -11,9 +17,9 @@ export class IsEmptyGuard implements CanActivate {
 		let body_request = text.body;
 
 		let password = body_request.password
-
-		let user_name = body_request.username
 		
+		let user_name = body_request.username
+				
 		// Invalid input.
 		if ((password === undefined) || (user_name === undefined)) {
 			return false;
---
+++ backend_prism/prism_backend/src/server/users/common/BodyFieldsUser.ts
@@ -0,0 +1,4 @@
+export type BodyFieldsUsers = {
+  username : "string",
+  password : "string"
+}
