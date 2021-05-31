1f0225cb511d9a3456823c1b6cc27fedeee8ff90
Dmytro <turovetsd@icloud.com>
2021-05-31 11:57:48 +0300

    display user data ( without colors) in table & react to changes in selection forms

--- frontend_prism/front_typescript/src/adminOperationSideBar/SoldierStatus/SoldierSubmissions.jsx
+++ frontend_prism/front_typescript/src/adminOperationSideBar/SoldierStatus/SoldierSubmissions.jsx
@@ -63,7 +63,8 @@ class SoldierSubmissions extends React.Component {
 		super(props);
 
 
-
+		this.modules = []
+		this.modules_subjects = {}
 
 		this.state = {
 			selectedMajor: this.props.selectedMajor,
@@ -71,7 +72,7 @@ class SoldierSubmissions extends React.Component {
 			
 			submissionData: undefined,
 
-			FOO: false
+			module_submissions: undefined
 
 		}
 
@@ -84,7 +85,7 @@ class SoldierSubmissions extends React.Component {
 
 		let arrPromises = []
 		console.log("3")
-		console.log("starting iterate module module & retrieve subjects of each module module")
+		//console.log("starting iterate module module & retrieve subjects of each module module")
 		for (const module of modules){
 			
 
@@ -113,6 +114,9 @@ class SoldierSubmissions extends React.Component {
 
 		let allRESOLVEV = Promise.all(arrPromises) 
 		console.log("all pormises are resolved")
+		
+
+		
 		console.log(allRESOLVEV)
 		return allRESOLVEV
 
@@ -126,22 +130,34 @@ class SoldierSubmissions extends React.Component {
 		// then () -> getSubjects
 		// then () -> submissions
 		console.log("1")
-		let selectedMajor = this.state.selectedMajor
-		console.log("Selected major is " + selectedMajor)
+		//let selectedMajor = this.state.selectedMajor
+		let selectedMajor = this.props.selectedMajor
+		//console.log("Selected major is " + selectedMajor)
 
 		getModulesByMajor(selectedMajor).then((response) => {
 			if (response !== undefined) {
 		  		let modules_ = response.data
 				console.log("2")
-				console.log("Arrived modules" )
-				console.log(modules_)
-				console.log("Starting extract subjects of arrived modules.....")
+				//console.log("Arrived modules" )
+				//console.log(modules_)
+				this.modules = modules_
+				//console.log("Starting extract subjects of arrived modules.....")
 				
 				this.getSubjectsOfModules(modules_, selectedMajor).then((modules_subjects)=>{
 
-					console.log(modules_subjects)
+					let mod_sub = {}
+					for (const dict_mod_sub of modules_subjects){
+
+						for (const [key, value] of Object.entries(dict_mod_sub)) {
+							mod_sub[key] = value
+						  } 
+					}
+
+					this.modules_subjects = mod_sub
+
+					//console.log(modules_subjects)
 					console.log("5")
-					console.log("END extract subjects of arrived modules.....?")
+					//console.log("END extract subjects of arrived modules.....?")
 					
 					this.getAllUserSubmissions(modules_subjects)
 					
@@ -158,28 +174,27 @@ class SoldierSubmissions extends React.Component {
 		let currentSoldier = this.props.selectedSoldier
 		let selectedMajor = this.props.selectedMajor
 		soldiers.push({personalId:currentSoldier})
+		console.log("Ask about soldier :: " + soldiers[0].personalId)
 
-		console.log(modules_subjects)
+		//console.log(modules_subjects)
 
 		let arrPromises = []
-		let modules_submissions = {}
+		let modules_submissions_ = {}
 
 		for (const module_sub_map of modules_subjects){
+			
 			for (const module in module_sub_map){
 				
 
 			let promise_submission = new Promise((resolv, rej)=>{
 				
 				
-			
-				setTimeout(function(){		
-				
-					usersSubmissions(soldiers,selectedMajor,module).then((res)=>{
+				usersSubmissions(soldiers,selectedMajor,module).then((res)=>{
 					if (res !== undefined){
 						if (res.data !== undefined){
 
-							modules_submissions[module] = res.data
-							resolv(modules_submissions)
+							modules_submissions_[module] = res.data
+							resolv(modules_submissions_)
 						}
 						else {
 							rej(undefined)
@@ -188,24 +203,20 @@ class SoldierSubmissions extends React.Component {
 						rej(undefined)
 					}
 
-					})
-				
-				}, 5000);
-
+				})
 			}) 
-
-
+			
 			arrPromises.push(promise_submission)	
 	
 			}
 			
 		}
 		console.log("7 waiting ...")
-		console.log(modules_submissions)
+		//console.log(modules_submissions_)
 		Promise.all(arrPromises)
-		console.log("all pormises are resolved")
-		console.log(modules_submissions)
-		
+		//console.log("all pormises are resolved")
+		//console.log(modules_submissions_)
+		this.setState({module_submissions: modules_submissions_})
 	}
 
 
@@ -214,43 +225,110 @@ class SoldierSubmissions extends React.Component {
 
 
 	componentDidUpdate() {
-
+		console.log("in updating ")
 		// if new soldier was selected
+		if (this.state.selectedMajor !== this.props.selectedMajor){
+			this.setState({selectedMajor : this.props.selectedMajor})
+		}
+
+		if (this.state.selectedSoldier !== this.props.selectedSoldier){
+			console.log("New soldier was chosen -- " + this.props.selectedSoldier)
+			
+			this.setState({selectedSoldier : this.props.selectedSoldier}, function() {
+				this.extractAllNeededData()
+			})
+			
+		}
         
         // if new major was selected
 	}
 
 
+	extractAllNeededData(){
+		this.getModulesOfMajor()
+	}
 
 	componentDidMount() {
-
-		this.getModulesOfMajor()
+		
+		this.extractAllNeededData()
+		
 	}
 
 
 
 	render() {
         
-        console.log("------in displaying submissions------")
-        console.log(this.props.selectedSoldier)
-        console.log(this.props.selectedMajor)
-		console.log(this.state.FOO)
-
-
+        //console.log("------in displaying submissions------")
+        //console.log(this.props.selectedSoldier)
+        //console.log(this.props.selectedMajor)
+		
         let classes = this.props.classes
 
         let allSoldierDisplay = []
 
-        let x = 1
+        let displayTable = (this.state.module_submissions !== undefined)
 
-		if (x) {
+		if (displayTable) {
 
 			//let personalIdColors = this.convertToColors(soldierSubmissionData)
+			
+			//console.log("^^^^^^^^^11^^^^^^^^^^^^^^")
+			//console.log(this.modules)
+			//console.log("^^^^^^^^^^^22^^^^^^^^^^^^")
+			//console.log(this.modules_subjects)
+
+			//console.log(this.modules[0])
+			//console.log(this.modules_subjects[this.modules[0]])
+			//console.log("^^^^^^^^^^^33^^^^^^^^^^^^")
+			console.log(this.state.module_submissions)
+			//console.log("^^^^^^^^^^^^^^^^^^^^^^^")
+			//console.log("^^^^^^^^^^^^^^^^^^^^^^^")
+			
+
 
 			return (
 
 
-			<h2> TABLE HERE </h2>
+
+						// stickyHeader
+						<Table className={classes.table} aria-label="simple table" >
+						
+						<TableBody>
+
+
+							{/*Vertical frozen bar of all soldiers*/}
+							{this.modules.map((module, index) => (
+								<TableRow key={module}>
+									<TableCell
+
+										className={classes.sticky}
+										component="th"
+										scope="row"
+									>
+										{
+											//this.modules_subjects[module]
+											module
+										}
+										
+									</TableCell>
+
+									{
+										this.modules_subjects[module].map((term) => (
+
+											<TableCell className={classes.tableCell}>
+
+												{term}
+												
+
+											</TableCell>
+										))
+
+									}
+								</TableRow>
+							))}
+						</TableBody>
+
+					</Table>
 
 
 
