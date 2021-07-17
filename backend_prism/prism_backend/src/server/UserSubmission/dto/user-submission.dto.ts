76a4efd9343ac4332ef8feaff8af4845f9d70f06
Dmytro <turovetsd@icloud.com>
2021-07-17 15:42:48 +0300

    artificially kety created ctf -- fixed

--- backend_prism/prism_backend/src/server/UserSubmission/dto/user-submission.dto.ts
+++ backend_prism/prism_backend/src/server/UserSubmission/dto/user-submission.dto.ts
@@ -1,23 +1,16 @@
 import { Major } from "../../users/common/major.enum";
-import { IsNotEmpty, IsNumberString, IsOptional } from "class-validator";
 
 
 export class UserSubmissionDTO {
     
-    @IsNumberString()
-    @IsNotEmpty()
     soldierId: string;
 
-    @IsNotEmpty()
     major: Major
 
-    @IsNotEmpty()
     module: string
 
-    @IsNotEmpty()
     subject: string 
 
-    @IsNotEmpty()
     isChecked: boolean;
 
     submittedFiles: string[];
